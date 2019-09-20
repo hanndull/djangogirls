95097640687e1a28c169a2309f9ab7c04c7ff364
Hannah Johnson <hstutzman@gmail.com>
2019-09-19 20:31:30 -0700

    Turning debug off for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$ofr-+efn*8mph8u#^#836y)6g$ioo=znrm2fi*ufie@h^cz8f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
